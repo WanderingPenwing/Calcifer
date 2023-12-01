72c30ffb2babad573767f02ac0c03e749d96be6c
WanderingPenwing <nicolas.pinson31@gmail.com>
2023-12-01 16:09:42 +0100

    added static tabs (for notes)

--- src/pages/notes.js
+++ src/pages/notes.js
@@ -1,11 +1,26 @@
+//<input type="file" name="inputfile" id="inputfile">
+
 document.getElementById('inputfile')
-            .addEventListener('change', function () {
+  .addEventListener('change', function () {
+    let fr = new FileReader();
+    fr.onload = function () {
+      document.getElementById('output').textContent = fr.result;
+    }
+    fr.readAsText(this.files[0]);
+})
 
-                let fr = new FileReader();
-                fr.onload = function () {
-                    document.getElementById('output')
-                        .textContent = fr.result;
-                }
+function openCity(evt, cityName) {
+  console.log("helooo")
+  // Declare all variables
+  var i, tablinks;
 
-                fr.readAsText(this.files[0]);
-            })
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
