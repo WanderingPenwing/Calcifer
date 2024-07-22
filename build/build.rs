1d13e429ff8701ecb84c0ab0a1c8cba90728da79
WanderingPenwing <nicolas.pinson31@gmail.com>
2024-07-22 20:36:00 +0200

    removed custom build.rs

--- build/build.rs
+++ build/build.rs
@@ -1,4 +1,4 @@
 fn main() {
-    // custom parameter to fix clipboard issue between calcifer and external
-    println!("cargo:rustc-cfg=web_sys_unstable_apis");
+	// custom parameter to fix clipboard issue between calcifer and external
+	println!("cargo:rustc-cfg=web_sys_unstable_apis");
 }
